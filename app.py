1e05e62ae8efdb7c3773fe33eaa2e3599a3afaae
Junaid Awan <awanj@cardiff.ac.uk>
2022-09-05 06:22:22 +0100

    UPDATED .env

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class MyEncoder(json.JSONEncoder):
 app = Flask(__name__)
 CORS(app)
 
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URl")
 mongo = PyMongo(app)
 
 app.json_encoder = MyEncoder
